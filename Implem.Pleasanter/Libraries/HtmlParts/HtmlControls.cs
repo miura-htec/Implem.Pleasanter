ccfb68e163eb0cdb7801de025509616d9dd9808e
Yuki Miura <yuki.miura@hightec.co.jp>
2026-02-17 16:35:16 +0900

    アイコンタイプのマッピングを更新（class → sell）

--- Implem.Pleasanter/Libraries/HtmlParts/HtmlControls.cs
+++ Implem.Pleasanter/Libraries/HtmlParts/HtmlControls.cs
@@ -1177,7 +1177,7 @@ namespace Implem.Pleasanter.Libraries.HtmlParts
             var iconTypesMap = new Dictionary<string, string>
             {
                 ["basic"] = "apps",
-                ["class"] = "text_fields",
+                ["class"] = "sell",
                 ["num"] = "timer_10",
                 ["date"] = "calendar_month",
                 ["description"] = "edit_note",
